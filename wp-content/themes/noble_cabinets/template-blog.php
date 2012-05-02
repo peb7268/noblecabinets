3e91627407eed2a8cda9b2fa08f24552d9a9e344
Paul Barrick <paul@imperativedesign.net>
2012-05-01 22:56:54 -0400

    added twitter and facebook btns

--- wp-content/themes/noble_cabinets/template-blog.php
+++ wp-content/themes/noble_cabinets/template-blog.php
@@ -17,9 +17,21 @@ get_header(); ?>
 				
 					<div class="social-sharing">
 						<div class="twitter-btn">
+						<a href="https://twitter.com/share" class="twitter-share-button" data-url="<?php the_permalink(); ?>">Tweet</a>
+						<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 							<script type="text/javascript" src="//platform.twitter.com/widgets.js"></script>
-						</div><!-- .twitter-btn -->					
-						<!-- .fblike goes here -->	
+						</div><!-- .twitter-btn -->
+						
+					<div class="fb-like" data-href="<?php the_permalink(); ?>" data-send="false" data-layout="button_count" data-width="450" data-show-faces="false" data-font="lucida grande"></div>						
+					
+					<div id="fb-root"></div>
+					<script>(function(d, s, id) {
+					var js, fjs = d.getElementsByTagName(s)[0];
+					if (d.getElementById(id)) return;
+					js = d.createElement(s); js.id = id;
+					js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=187996341275269";
+					fjs.parentNode.insertBefore(js, fjs);
+					}(document, 'script', 'facebook-jssdk'));</script>
 					</div><!-- .social-sharing -->
 					
 					<?php the_content(); ?>
