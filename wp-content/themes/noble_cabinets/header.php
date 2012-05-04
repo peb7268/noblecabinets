df140ac806d89ff352b22c9ece1e3b62c944f61a
Paul Barrick <paul@imperativedesign.net>
2012-05-04 18:32:08 -0400

    blog css

--- wp-content/themes/noble_cabinets/header.php
+++ wp-content/themes/noble_cabinets/header.php
@@ -13,7 +13,7 @@
 <!--[if lt IE 7]> <html class="ie6 ie"> <![endif]-->
 <!--[if IE 7]>    <html class="ie7 ie"> <![endif]-->
 <!--[if IE 8]>    <html class="ie8 ie"> <![endif]-->
-<!--[if IE IE 9]>	<html class="ie9 ie"> <!--<![endif]-->
+<!--[if IE 9]>	<html class="ie9 ie"> <!--<![endif]-->
 <!--[if gt IE 9]><!--> <html> <!--<![endif]-->
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
