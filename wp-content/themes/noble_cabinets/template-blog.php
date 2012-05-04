601d0ec369a329e2e937ff502b68c65fb203e898
Paul Barrick <paul@imperativedesign.net>
2012-05-04 18:46:11 -0400

    blog css

--- wp-content/themes/noble_cabinets/template-blog.php
+++ wp-content/themes/noble_cabinets/template-blog.php
@@ -56,7 +56,7 @@
 							}(document, 'script', 'facebook-jssdk'));</script>
 							</div><!-- .social-sharing -->
 							
-							<?php the_content('Read More..'); ?>
+							<?php the_content('Read More &raquo; &raquo;'); ?>
 		
 						</div><!-- .theContent -->
 		
