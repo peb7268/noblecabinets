3fd949f949267796c482e71a37a4cc528ce15419
Paul Barrick <paul@imperativedesign.net>
2012-05-04 20:13:59 -0400

    images path changes

--- wp-content/themes/noble_cabinets/sidebar.php
+++ wp-content/themes/noble_cabinets/sidebar.php
@@ -1,9 +1,9 @@
 <div id="sidebar">
 <ul class="sidebar-block">	 
 	<ul id="ads">
-		<li id="ad-banner" class="large"><a href="#"></a></li>
-		<li id="ad-1"><a href="#"></a></li>
-		<li id="ad-2"><a href="#"></a></li>
+		<li id="ad-banner" class="large"><a href="/blog/how-home-improvements-can-save-you-money"></a></li>
+		<li id="ad-1"><a href="/blog/a-noble-refferal/"></a></li>
+		<li id="ad-2"><a href="/client-testimonials"></a></li>
 	</ul>
 	<div class="clear">&nbsp;</div>
 	<?php if ( ! dynamic_sidebar( 'primary-widget-area' ) ) : ?>
