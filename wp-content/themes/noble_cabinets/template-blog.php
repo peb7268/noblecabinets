ebd10302edf9f07ec2f1986abff51f80873ba8fd
Paul Barrick <paul@imperativedesign.net>
2012-05-04 20:56:54 -0400

    default featured image

--- wp-content/themes/noble_cabinets/template-blog.php
+++ wp-content/themes/noble_cabinets/template-blog.php
@@ -27,8 +27,8 @@
 							if ( has_post_thumbnail() ) {
 								echo get_the_post_thumbnail();
 							} else {
-								
-								echo "<img src=/images/default_featured_img.png />";
+								$img = get_bloginfo('stylesheet_directory')."/images/default_featured_img.png";
+								echo "<img src=".$img."/>";
 							}
 						?>		
 				</div><!-- .featured_img .left-->
